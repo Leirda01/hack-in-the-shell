b3ee0290a59d5a16fca49bf5073ff2ccc36002d3
Leirda01 <adrieldj@orange.fr>
2020-03-04 11:12:34 +0000

    Teste de plus en plus de cas

--- hack.sh
+++ hack.sh
@@ -1,19 +1,37 @@
 #!/usr/bin/env bash
 
-function treasy {
-  echo -en "$1\t" | tr $2 $3
-}
+function try { echo -en "$1\n"; }
+function treasy { try $1 | tr $2 $3; }
+
+function looooower { treasy $1 [:upper:] [:lower:]; }
+function UUUUUPPER { treasy $1 [:lower:] [:upper:]; }
+function inversion { treasy $1 [:lower:][:upper:] [:upper:][:lower:]; }
 
 function LoWeRUpPeR {
-  treasy $1 [:lower:] [:upper:] # hands up!
-  treasy $1 [:upper:] [:lower:] # hands down!
-  treasy $1 [:lower:][:upper:] [:upper:][:lower:] # invert
-  treasy $1 [:lower:] [:upper:] | sed -r 's/(.)(.)/\l\1\2/g' # alternate
-  treasy $1 [:lower:] [:upper:] | sed -r 's/(.)(.)/\1\l\2/g' # the other way
+  looooower $1
+  UUUUUPPER $1
+  inversion $1
+  UUUUUPPER $1 | sed -r 's/(.)(.)/\l\1\2/g' # BaSh…
+  UUUUUPPER $1 | sed -r 's/(.)(.)/\1\l\2/g' # bAsH…
+  looooower $1 | sed -r 's/./\u&/' # Bash…
+}
+
+function l33t {
+  for many in $(LoWeRUpPeR $1); do
+    treasy $many oieasgtOIEASGT 01345670134567
+  done
+}
+
+function express {
+  for many in $(LoWeRUpPeR $1; l33t $1); do
+    echo $many | sed -r 's/$/:)/'
+    echo $many | sed -r 's/$/!/'
+  done
 }
 
 for pass in $(cat $1); do
-  echo -en "$pass\t"
+  try $pass
   LoWeRUpPeR $pass;
-  echo
+  l33t $pass;
+  express $pass;
 done
